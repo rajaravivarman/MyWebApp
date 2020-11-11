c653723d448f20f798c8c92d4fbef39cc81b0700
Raja Ravi Varman <email@rajaravivarman.com>
2020-11-11 17:15:08 +0530

    first commit

---
+++ backend.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+for (man in men) {
+  a = 5;
+}
---
+++ funcs.js
@@ -0,0 +1,6 @@
+var raja = 0;
+function setTextRaja() {
+  document.getElementById("raja").innerText =
+    "Button pressed " + ++raja + " times";
+  // raja = raja + 1;
+}
